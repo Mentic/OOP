ada6c9fb978868d42d9b34862f28b0698d95f492
Vacariu Andrei R15 <andrei.vacariu123@gmail.com>
2026-02-18 23:57:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,22 @@
-#include <cstdio>
 #include <iostream>
+#include <fstream>
+
 using namespace std;
- char text[100];
-char copie[100][100];
-
-
+ifstream fin("text.txt");
 int main() {
 
-    scanf(" %99[^\n]", text);
-    int i=0;
-    int k=0;
-    int j=0;
-    int words = 0;
-    for (i=0;i<strlen(text);i++) {
-
-        if (text[i]==' ') {
-                j++;
-                k=0;
-                words++;
-        }else {
-            copie[j][k]=text[i];
-            k++;
-        }
+    if (!fin.is_open()) {
+        cout << "dawda";
+        return 0;
     }
-
-    for (int i = 0; i<=words;i++)
-        {
-        int ultim = words-1;
-        while (ultim > 0)
-            {
-             int n = ultim - 1;
-             ultim = 0;
-            for (j=0;j<=words;j++) {
-                if (strlen(copie[j])<strlen(copie[j+1])) {
-                    swap(copie[j], copie[j+1]);
-                    ultim = j;
-                }
-            }
-        }
-    }
-    for (int i = 0; i<=words;i++) {
-        cout<<copie[i]<<endl;
-    }
-
+    int x;
+    int sum = 0;
+    while(fin>>x)
+        sum +=x;
+    cout<<sum;
+    fin.close();
     return 0;
+
+
 }
+
